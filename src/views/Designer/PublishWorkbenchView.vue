4fe7e2828ecbc856d2dba659f9d2d6ed0647a2fb
dlsccc <i.dls@hotmail.com>
2026-04-17 11:19:57 +0800

    修复发布页面滚动，支持内嵌页面下滑查看

--- src/views/Designer/PublishWorkbenchView.vue
+++ src/views/Designer/PublishWorkbenchView.vue
@@ -94,6 +94,10 @@ const goBack = () => router.push('/designer');
   padding: 24px;
   max-width: 1280px;
   margin: 0 auto;
+  height: calc(100vh - 64px);
+  overflow-y: auto;
+  overflow-x: hidden;
+  padding-bottom: 32px;
 }
 
 .publish-header {
